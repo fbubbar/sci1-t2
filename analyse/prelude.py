fb3205944d0cf18e931de9b4a74a671c0071630c
Felix Bubbar <felix@bubbar.dev>
2025-03-09 19:42:24 +0000

    add moi results as constants for import

--- analyse/prelude.py
+++ analyse/prelude.py
@@ -2,7 +2,26 @@ from .period import *
 from .moi import *
 from .process import *
 from .num_sim import *
+
 # define constants
 PHONE_MASS = 0.17228 # [kg]
 PHONE_DIMS = np.array([71.5, 146.7, 7.4]) * 1e-3 # [m]
+
+
+# results for the phone-racquet system
+# see `moi` directory for the calculations
+
+PRQ_MASS = 0.285 + 0.17228 # [kg]
+PRQ_I = np.array([
+    1.8815656991e-02, # x-axis (from physical pendulum)
+    1.6149605212e-03, # y-axis (from torsional pendulum)
+    2.0703308161e-02, # z-axis (from physical pendulum)
+])
+
+# uncertainties for `PRQ_I`
+PRQ_DI = np.array([
+    5.6770997052e-03,
+    8.8317758313e-05,
+    3.8411877481e-03,
+])
 
