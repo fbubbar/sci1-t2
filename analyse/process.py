9460edc442f66cb78d48d852150b6454465091b2
Felix Bubbar <felix@bubbar.dev>
2025-03-10 18:54:56 +0000

    update combined notebook

--- analyse/process.py
+++ analyse/process.py
@@ -136,7 +136,6 @@ def plot_trial(i, trial, meta, cols, ylabel, with_absolute=True):
     ax.legend(legend)
     ax.set_ylabel(ylabel)
     plt.title(title)
-    plt.savefig(f'{rootpath}/figs/{source}_{j}.svg')
     plt.show()
 
 
